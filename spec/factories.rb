e57a7a313a9ceb9cb529214a3af159f7d2fe9a5f
kouchchivorn <kouchchivorn5@yahoo.com>
2014-10-06 22:29:42 +0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+    factory :user do
+        name  "Kouch Chivorn"
+        email  "chivorn@kouch.com"
+        password  "foobar"
+        password_confirmation  "foobar"
+    end
+end
